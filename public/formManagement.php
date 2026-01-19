baade75a462302a0abb8f6771823fc855057d826
thomaslucking <telucking@gmail.com>
2026-01-19 10:57:36 +0100

    feat: finished form management exercise

--- public/formManagement.php
+++ public/formManagement.php
@@ -14,15 +14,62 @@
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Form management</title>
 </head>
+
 <body>
 
-<!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
+    <!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
+
+    <?php
+    $name = "";
+    $age = "";
+    $fullagecheck = "";
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name_value"];
+        $age = $_POST["age_value"];
+
+        $color = (strlen($name) > 6) ? 'red' : 'black';
+        $fullagecheck = "<ul>";
+        for ($i = 1; $i <= $age; $i++) {
+            // the .= appends instead of replacing the data.
+            $fullagecheck .= "<li>" . $i . "</li>";
+        }
+        $fullagecheck .= "</ul>";
+        if (empty($name) || empty($age)) {
+            $headingText = "Submit the form";
+        } else {
+            $headingText = "<span style='color:$color;'>$name</span> is $age years old";
+
+        }
+    } else {
+        $headingText = "Submit the form";
+    }
+    ?>
+
+
+
+    <h1> <?php echo $headingText ?></h1>
+    <?php echo $fullagecheck ?>
+    <form method="POST">
+        <label for="user_n">Name</label>
+        <input type="text" id="user_n" name="name_value" value="<?php echo $_POST['name_value']; ?>">
+
+        <label for="user_a">Age</label>
+        <input type="text" id="user_a" name="age_value" value="<?php echo $_POST['age_value'] ?>">
+
+        <button type="submit">Submit</button>
+    </form>
+
+
 </body>
+
 </html>
