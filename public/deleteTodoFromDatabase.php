f361efb5f1b738f6a66dbb20f38af89830e091b3
thomaslucking <telucking@gmail.com>
2026-01-19 10:58:56 +0100

    feat: finished deleting all todos

--- public/deleteTodoFromDatabase.php
+++ public/deleteTodoFromDatabase.php
@@ -11,22 +11,51 @@
 ?>
 
 
+<?php
+
+$pdo = databaseconnection();
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $action = filter_input(INPUT_POST, 'action');
+    try {
+        if ($action == 'delete') {
+            $idtodelete = filter_input(INPUT_POST, 'id', filter: FILTER_SANITIZE_NUMBER_INT);
+
+            $deletetodoquery = $pdo->prepare("delete from todos where id = ?");
+            $deletetodoquery->execute([$idtodelete]);
+
+            header('location: ./displayAllTodosFromDatabase.php');
+            exit();
+        }
+    } catch (PDOException $e) {
+        echo '' . $e->getMessage();
+    }
+
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todo deletion</title>
 </head>
+
 <body>
 
-<h1>Delete a todo error</h1>
+    <h1>Delete a todo error</h1>
 
-<!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
+    <!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
 
-<a href="displayAllTodosFromDatabase.php">Return to todo list</a>
+    <a href="displayAllTodosFromDatabase.php">Return to todo list</a>
 
 </body>
+
 </html>
